5040a66e19e59677c4e45d4e9ef7cb29a906e76a
Mattia Belletti <MBelletti@greenteam.it>
2019-08-27 16:51:55 +0200

    Static number generator.

--- src/pseudo-random-generator.js
+++ src/pseudo-random-generator.js
@@ -4,6 +4,16 @@ import { TFGen } from "@susisu/tf-random";
 import splitNumber from "./split-number";
 
 /**
+ * @callback NextFunction
+ * @returns [number, SplittableRandomNumberGenerator]
+ */
+
+/**
+* @callback SplitFunction
+* @returns [SplittableRandomNumberGenerator, SplittableRandomNumberGenerator]
+*/
+
+/**
  * A splittable random number generator.
  * @typedef {Object} SplittableRandomNumberGenerator
  * @property {() => [number, SplittableRandomNumberGenerator]} next - gets a random number and the next generator.
@@ -24,3 +34,24 @@ export const createRandomGenerator = seed => {
 export const defaultRandomGenerator = createRandomGenerator(
   Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
 );
+
+/**
+ * Create a new random number generator that always returns the same set of values, cyclically.
+ * @param {number[]} values All the values returned by this generator
+ * @returns {SplittableRandomNumberGenerator} A static number generator.
+ */
+export const createStaticGenerator = values => {
+  /**
+   * @param {number} x
+   */
+  const crop = x => x % values.length;
+  /**
+   * @param {number} i
+   * @param {number} delta
+   */
+  const createGenerator = (i, delta) => ({
+    next: /** @type NextFunction */ () => [values[i], createGenerator(crop(i + delta), delta)],
+    split: /** @type SplitFunction */ () => [createGenerator(i, delta * 2), createGenerator(crop(i + delta), delta * 2)]
+  });
+  return createGenerator(0, 1);
+}
--- src/pseudo-random-generator.spec.js
+++ src/pseudo-random-generator.spec.js
@@ -1,5 +1,5 @@
 import { describe } from "riteway";
-import { createRandomGenerator, SplittableRandomNumberGenerator } from "./pseudo-random-generator";
+import { createRandomGenerator, createStaticGenerator, SplittableRandomNumberGenerator } from "./pseudo-random-generator";
 
 /**
  * 
@@ -12,31 +12,53 @@ const callN = (n, rng) => {
 }
 
 describe("pseudo-random-generator", async assert => {
-  const seed1 = 94935873482;
-  const gen1 = createRandomGenerator(seed1);
-  const values1 = callN(9, gen1);
-  const gen2 = createRandomGenerator(seed1);
-  const values2 = callN(9, gen2);
-  assert({
-    given: "two pseudo random number generators from the same seed",
-    should: "produce the same set of seeds",
-    actual: values2,
-    expected: values1
-  });
+  {
+    const seed1 = 94935873482;
+    const gen1 = createRandomGenerator(seed1);
+    const values1 = callN(9, gen1);
+    const gen2 = createRandomGenerator(seed1);
+    const values2 = callN(9, gen2);
+    assert({
+      given: "two pseudo random number generators from the same seed",
+      should: "produce the same set of seeds",
+      actual: values2,
+      expected: values1
+    });
 
-  const [gen11, gen12] = gen1.split();
-  const [gen21, gen22] = gen2.split();
-  const [values11, values12, values21, values22] = [gen11, gen12, gen21, gen22].map(g => callN(9, g));
-  assert({
-    given: "a split of two rng from the same seed",
-    should: "produce the same 'first' sub-generator",
-    actual: values21,
-    expected: values11
-  });
-  assert({
-    given: "a split of two rng from the same seed",
-    should: "produce the same 'second' sub-generator",
-    actual: values22,
-    expected: values12
-  });
+    const [gen11, gen12] = gen1.split();
+    const [gen21, gen22] = gen2.split();
+    const [values11, values12, values21, values22] = [gen11, gen12, gen21, gen22].map(g => callN(9, g));
+    assert({
+      given: "a split of two rng from the same seed",
+      should: "produce the same 'first' sub-generator",
+      actual: values21,
+      expected: values11
+    });
+    assert({
+      given: "a split of two rng from the same seed",
+      should: "produce the same 'second' sub-generator",
+      actual: values22,
+      expected: values12
+    });
+  }
+
+  {
+    const gen1 = createStaticGenerator([4, 2, 3, 5, 7]);
+    const values = callN(11, gen1);
+    assert({
+      given: "a static number generator",
+      should: "produce always the same sequence of values",
+      actual: values,
+      expected: [4, 2, 3, 5, 7, 4, 2, 3, 5, 7, 4]
+    });
+
+    const gens = gen1.split();
+    const [values1, values2] = gens.map(gen => callN(3, gen));
+    values1.forEach((value, i) => assert({
+      given: "a splitted static number generator",
+      should: "produce distinct values",
+      actual: value != values2[i],
+      expected: true
+    }));
+  }
 })
